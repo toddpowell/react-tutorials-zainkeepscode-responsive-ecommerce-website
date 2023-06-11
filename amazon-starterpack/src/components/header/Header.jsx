15089a6f87121efb8cce3bf6dde71f34c05be327
Todd Powell <toddwpowell@gmail.com>
2023-06-10 22:15:30 -0600

    Fix menu alignment. Add shopping bag logo.

--- amazon-starterpack/src/components/header/Header.jsx
+++ amazon-starterpack/src/components/header/Header.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import css from './Header.module.css'
+import Logo from '../../assets/logo.png';
+import {CgShoppingBag} from 'react-icons/cg'
 
 const Header = () => {
   return (
     <div className={css.container}>
       <div className={css.logo}>
-        Logo
+        <img src={Logo} alt="Logo" />
+        <span>amazon</span>
       </div>
 
       <div className={css.right}>
         <div className={css.menu}>
-
+          <ul className={css.menu}>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>ENG</li>
+          </ul>
         </div>
         
         <input type="text" className={css.search} />
 
-        <span>PNG icon of cart</span>
+        <CgShoppingBag className={css.cart}/>
       </div>
     </div>
   )
