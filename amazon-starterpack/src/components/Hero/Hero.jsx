4ee0cb35e723e66840f614bb42b1a302dfb98b7a
Todd Powell <toddwpowell@gmail.com>
2023-06-14 13:58:02 -0600

    Scaffold Hero left and center

---
+++ amazon-starterpack/src/components/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import css from './Hero.module.css'
+import HeroImg from '../../assets/hero.png'
+import {RiShoppingBagFill} from 'react-icons/ri'
+import {BsArrowRight} from 'react-icons/bs'
+
+
+const Hero = () => {
+  return (
+    <div className={css.container}>
+
+        {/* left side */}
+        <div className={css.h_sides}>
+            <span>skin protection cream</span>
+            <div className={css.text2}>
+                <span>Trendy Collection</span>
+                <span>Lorem ipsum text goes here</span>
+            </div>
+        </div>
+
+        {/* center */}
+        <div className={css.wrapper}>
+            <div className={css.blueCircle}></div>
+            <img src={HeroImg} alt="" width={600} />
+            <div className={css.cart2}>
+                <RiShoppingBagFill/>
+                <div className={css.signup}>
+                    <span>Best Signup Offers</span>
+                </div>
+                <div>
+                    <BsArrowRight/>
+                </div>
+            </div>
+        </div>
+        {/* right side */}
+    </div>
+  )
+}
+
+export default Hero
