7cc11cdc757dbeb246381e9b27c60565178492a5
Todd Powell <toddwpowell@gmail.com>
2023-06-25 17:03:26 -0600

    Slider css - update left side, name, and details

--- amazon-starterpack/src/components/Slider/Slider.jsx
+++ amazon-starterpack/src/components/Slider/Slider.jsx
@@ -13,13 +13,18 @@ import {SliderProducts} from '../../data/products';
 const Slider = () => {
   return (
     <div className="s-container">
-        <Swiper slidesPerView={3} spacesBetween={40} slidesPerGroup={1} loop={true}>
+        <Swiper 
+            modules={[Pagination, Navigation]}
+            navigation={true}
+            className='mySwiper'
+            loopFillGroupWithBlank={true}
+            slidesPerView={3} spacesBetween={40} slidesPerGroup={1} loop={true}>
             {SliderProducts.map((slide, i) => (
                <SwiperSlide>
                     <div className="left-s">
                         <div className="name">
                             <span>{slide.name}</span>
-                            <span>{slide.name}</span>
+                            <span>{slide.detail}</span>
                         </div>
 
                         <span>${slide.price}</span>
