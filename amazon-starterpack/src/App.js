853b58fa86b0ef41a459041a4c4d4061e61d343c
Todd Powell <toddwpowell@gmail.com>
2023-07-12 10:45:59 -0600

    Start Products component

--- amazon-starterpack/src/App.js
+++ amazon-starterpack/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/header/Header";
 import Hero from './components/Hero/Hero';
 import Slider from './components/Slider/Slider';
 import Virtual from "./components/Virtual/Virtual";
+import Products from "./components/Products/Products";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero/>
       <Slider/>
       <Virtual/>
+      <Products/>
     </div>
   );
 }
---
+++ amazon-starterpack/src/components/Products/Products.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import css from './Products.module.css'
+import Plane from '../../assets/plane.png'
+import {ProductsData} from "../../data/products"
+
+const Products = () => {
+  const [MenuProducts, setMenuProducts] = useState(ProductsData)
+
+  return (
+    <div className={css.container}>
+        <img src={Plane} alt="" />
+        <h1>Our Featured Products</h1>
+
+        <div className={css.products}>
+            <ul className={css.menu}>
+                <li>All</li>
+                <li>Skin Care</li>
+                <li>Conditioners</li>
+                <li>Foundations</li>
+            </ul>
+        </div>
+    
+    <div className={css.list}>
+        {
+            MenuProducts.map((product, i) => (
+                <div className={css.product}>
+                    Products
+                </div>
+            )
+
+            )
+        }
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Products
